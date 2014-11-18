b51efa4059691c4d15c130f56e2b6fa2dc26a25d
Adam <adam.gauthier1@verizon.net>
2014-11-18 14:50:53 -0500

    User added

--- blog/forms.py
+++ blog/forms.py
@@ -21,4 +21,4 @@ class UserForm(forms.ModelForm):
 class EntryForm(forms.ModelForm):
     class Meta:
 	model = Entry
-	fields = ['title', 'slug', 'body']
+	fields = ['title', 'slug', 'body','myUploadFile']
