cb9ce358959c6de322cd71e0fd2692d30cf5beb2
Austin Lamon <a.lamon4@gmail.com>
2014-12-03 18:24:41 -0500

    adding folder ui, and feed information

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
+                ('firstName', models.CharField(max_length=40)),
+                ('lastName', models.CharField(max_length=40)),
+                ('username', models.CharField(unique=True, max_length=40)),
+                ('admin', models.BooleanField(default=False)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Entry',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('slug', models.SlugField(unique=True, max_length=200)),
+                ('publish', models.BooleanField(default=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('myFile', models.FileField(default=None, upload_to=b'files')),
+                ('body', models.TextField()),
+                ('has_enc', models.CharField(default=b'False', max_length=5)),
+                ('event_location', models.CharField(default=b' ', max_length=1000)),
+                ('author', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'ordering': ['-created'],
+                'verbose_name': 'Blog Entry',
+                'verbose_name_plural': 'Blog Entries',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.SlugField(unique=True, max_length=200)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='entry',
+            name='tagline',
+            field=models.ForeignKey(blank=True, to='blog.Tag', null=True),
+            preserve_default=True,
+        ),
+    ]
